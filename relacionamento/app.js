843d2865d0f8598a03470d6ca1d8aad9de702788
Sheldon441 <sheldonmarcos021@gmail.com>
2024-08-17 15:09:17 -0300

    Add files via upload

--- relacionamento/app.js
+++ relacionamento/app.js
@@ -1,25 +1,21 @@
 const prompt = require('prompt-sync')();
 
 const criarBaralho = require('./baralho/criarBaralho');
-const removerBaralho = require('./baralho/removerBaralho');
-const listarBaralho = require('./baralho/listarBaralhos');
+const criarFlashcard = require('./flashcard/criarFlashcard');
+const listarBaralhos = require('./baralho/listarBaralhos');
+const listarFlashcards = require('./flashcards/listarFlashCards'); // Corrigido: 'flashcards' para 'flashcard'
+const listarFlashcardsPorBaralhoId = require('./flashcard/listarFlashcardsPorBaralho');
 const atualizarBaralho = require('./baralho/atualizarBaralho');
-const atualizarFlashcard = require ('./flashcard/atualizarFlashcard');
-const buscarFlashcardsPorBaralho = require ('./flashcard/buscarFlashcardsPorBaralho');
-const buscarFlashcardsPorPergunta = require ('./flashcard/buscarFlashCardsPorPergunta');
-const criarFlashcard= require ('./flashcard/criarFlashcard');
-const deletarFlashcard = require ('./flashcard/deletarFlashcard');
-const findFlashcardIndexById = require ('./flashcard/listarFlashcardIndexById');
-const listarFlashcards = require ('./flashcards/listarFlashCards');
-const listarFlashcardsPorBaralhoId = require ('./flashcard/listarFlashcardsPorBaralho');
+const atualizarFlashcard = require('./flashcard/atualizarFlashcard');
+const deletarBaralho = require('./baralho/deletarBaralho');
+const deletarFlashcard = require('./flashcard/deletarFlashcard');
+const buscarFlashcardsPorPergunta = require('./flashcard/buscarFlashCardsPorPergunta');
+const buscarFlashcardsPorBaralho = require('./flashcard/buscarFlashcardsPorBaralho');
 
 menu();
 
-
-  function menu() {
-
+function menu() {
     console.log(`
-    
     Escolha uma opção:
 
     1. Criar Baralho
@@ -34,46 +30,78 @@ menu();
    10. Buscar Flashcards por Pergunta
    11. Buscar Flashcards por Baralho
    0. Sair
-
-    `)
-    const opcao = prompt ('Escolha uma opção:')
+    `);
     
-    let index;
-
+    const opcao = prompt('Escolha uma opção:');
+    
     switch (opcao) {
-
-      case '1':
-            const titulo = prompt('Digite o título do baralho ');
-                    criarBaralho({ titulo });
-                      console.log('Baralho adicionado com sucesso !');
-                        menu();
-                          break;
+        case '1':
+            const titulo = prompt('Digite o título do baralho: ');
+            criarBaralho({ titulo });
+            console.log('Baralho adicionado com sucesso!');
+            menu();
+            break;
         case '2':
-            listarFlashcards();
-              const pergunta = prompt('Digite a pergunta: ');
-                    criarFlashcard({ pergunta });
-                      console.log('Pergunta adicionada com sucesso !');
-                        menu();
-                        break;
+            const pergunta = prompt('Digite a pergunta: ');
+            criarFlashcard({ pergunta });
+            console.log('Flashcard adicionado com sucesso!');
+            menu();
+            break;
         case '3':
             listarBaralhos();
-               const novoTitulo = prompt('Digite o título do baralho: ');
-                    atualizar(index, { titulo: novoTitulo }); //id antes do novo baralho
-                    console.log('Baralho atualizado com sucesso !')
-                    menu();
-                    break;
+            menu();
+            break;
         case '4':
-            
+            listarFlashcards();
+            menu();
+            break;
+        case '5':
+            const baralhoId = prompt('Digite o ID do baralho para listar os flashcards: ');
+            listarFlashcardsPorBaralhoId(baralhoId);
+            menu();
+            break;
+        case '6':
+            const idBaralhoParaAtualizar = prompt('Digite o ID do baralho que deseja atualizar: ');
+            const novoTituloBaralho = prompt('Digite o novo título do baralho: ');
+            atualizarBaralho(idBaralhoParaAtualizar, { titulo: novoTituloBaralho });
+            console.log('Baralho atualizado com sucesso!');
+            menu();
+            break;
+        case '7':
+            const idFlashcardParaAtualizar = prompt('Digite o ID do flashcard que deseja atualizar: ');
+            const novaPergunta = prompt('Digite a nova pergunta do flashcard: ');
+            atualizarFlashcard(idFlashcardParaAtualizar, { pergunta: novaPergunta });
+            console.log('Flashcard atualizado com sucesso!');
+            menu();
+            break;
+        case '8':
+            const idBaralhoParaRemover = prompt('Digite o ID do baralho que deseja remover: ');
+            deletarBaralho(idBaralhoParaRemover);
+            console.log('Baralho removido com sucesso!');
+            menu();
+            break;
+        case '9':
+            const idFlashcardParaDeletar = prompt('Digite o ID do flashcard que deseja deletar: ');
+            deletarFlashcard(idFlashcardParaDeletar);
+            console.log('Flashcard deletado com sucesso!');
+            menu();
             break;    
+        case '10':
+            const perguntaBusca = prompt('Digite a pergunta para buscar flashcards: ');
+            buscarFlashcardsPorPergunta(perguntaBusca);
+            menu();
+            break;
+        case '11':
+            const idBaralho = prompt('Digite o ID do baralho para buscar seus flashcards: ');
+            buscarFlashcardsPorBaralho(idBaralho);
+            menu();
+            break;
+        case '0':
+            console.log('O sistema será encerrado.');
+            break;
         default:
             console.log('Opção inválida, tente novamente.');
             menu();
-
+            break;
+    }
 }
-}
-
-                   const novaPergunta = prompt('Digite a nova pergunta do flashcard: ');
-                   atualizar(index, { pergunta: novaPergunta }); //id antes do novo baralho
-                   console.log('Pergunta atualizada com sucesso !')
-                   menu();
-                   break;
